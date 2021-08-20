3a8c1b2eab9160b350221cd6db5e4ebf0036ead6
khanasif1 <khanasif1@gmail.com>
2021-08-20 14:04:08 +1000

    update

--- _azure/_aks_artifacts/kubectl/deploycomponents.ps1
+++ _azure/_aks_artifacts/kubectl/deploycomponents.ps1
@@ -6,21 +6,14 @@ az aks check-acr --name $CLUSTER_NAME  --resource-group $RESOURCE_GROUP_NAME --a
 kubectl create namespace ap
 kubectl label namespace ap location=nsap
 
-kubectl label namespace app location=nsapp
+kubectl create namespace app2
 kubectl label namespace app2 location=nsapp2
 <#Deploy NGINX#>
-kubectl run nginxpod --image=nginx --namespace=app
-kubectl apply -f _azure/_aks_artifacts/yaml/application/nginx.yaml
-kubectl apply -f _azure/_aks_artifacts/yaml/application/nginx_ns2.yaml
-
-<#Deploy App#>
-kubectl apply -f _azure/_aks_artifacts/yaml/application/web.yaml
-kubectl apply -f _azure/_aks_artifacts/yaml/application/product.yaml
-kubectl apply -f _azure/_aks_artifacts/yaml/application/product_ns2.yaml
-
+kubectl apply -f _azure/_aks_artifacts/namespace_policy/yaml/application/frontend.yaml
+kubectl apply -f _azure/_aks_artifacts/namespace_policy/yaml/application/backend.yaml
 
 <#Deploy Policy#>
-kubectl apply -f _azure/_aks_artifacts/yaml/policy/blockall_productpolicy.yaml
-kubectl apply -f _azure/_aks_artifacts/yaml/policy/allow_nginx_product.yaml
-kubectl apply -f _azure/_aks_artifacts/yaml/policy/allow_cross_ns_pod.yaml
+kubectl apply -f _azure/_aks_artifacts/namespace_policy/yaml/policy/blockall_productpolicy.yaml
+kubectl apply -f _azure/_aks_artifacts/namespace_policy/yaml/policy/allow_nginx_product.yaml
+kubectl apply -f _azure/_aks_artifacts/namespace_policy/yaml/policy/allow_cross_ns_pod.yaml
 
