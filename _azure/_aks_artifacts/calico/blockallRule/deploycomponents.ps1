c7e17223c8bb032dc44b784651dc9f573ce07628
khanasif1 <khanasif1@gmail.com>
2021-08-20 16:56:23 +1000

    blockall

--- _azure/_aks_artifacts/calico/blockallRule/deploycomponents.ps1
+++ _azure/_aks_artifacts/calico/blockallRule/deploycomponents.ps1
@@ -3,11 +3,11 @@ $CLUSTER_NAME="aks-calico-demo"
 
 az aks check-acr --name $CLUSTER_NAME  --resource-group $RESOURCE_GROUP_NAME --acr msdemoregistry.azurecr.io
 
-kubectl create namespace ap
-kubectl label namespace ap location=nsap
+kubectl create namespace blockap
+kubectl label namespace blockap location=nsap
 
-kubectl create namespace app2
-kubectl label namespace app2 location=nsapp2
+kubectl create namespace blockapp2
+kubectl label namespace blockapp2 location=nsapp2
 <#Deploy NGINX#>
 kubectl apply -f _azure/_aks_artifacts/calico/namespaceRule/application/frontend.yaml
 kubectl apply -f _azure/_aks_artifacts/calico/namespaceRule/application/frontend_side.yaml
