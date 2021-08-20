acb6a2388a1d6680ef36d8550f3237bd4ff0093e
khanasif1 <khanasif1@gmail.com>
2021-08-20 12:05:37 +1000

    update

--- _azure/_aks_artifacts/kubectl/clusterquery.ps1
+++ _azure/_aks_artifacts/kubectl/clusterquery.ps1
@@ -2,43 +2,67 @@ kubectl config set-context --current --namespace=app
 
 kubectl get ns
 kubectl get nodes
-kubectl get ns
+kubectl get ns -A
+
+#kubectl delete deployment product-2 --namespace app2
+#kubectl delete service backend --namespace app
+#kubectl delete ns app
+
+kubectl get deployment  -A
+#kubectl delete deployment nginx --namespace app
+#kubectl delete deployment nginx-2 --namespace app2
+#kubectl delete deployment web --namespace app
+#kubectl delete deployment product --namespace app
 #kubectl delete deployment product-2 --namespace app2
 
 kubectl get deployment  -A
 kubectl get pod -o wide -A
-kubectl describe pod product-2-5cf5658cf6-j6nkl --namespace app2
+kubectl get service -o wide -A
+kubectl describe pod nginx-2-549dbdf9d9-l577h --namespace app2
 kubectl describe deployment   product-2 --namespace app2
+
+kubectl describe namespace app2
 
 kubectl get NetworkPolicy -A -o wide
 kubectl describe NetworkPolicy allow-nginx-product
 kubectl describe NetworkPolicy blockallpolicy
+kubectl describe NetworkPolicy allow-metrics-prom --namespace app
 
 #kubectl delete networkpolicy blockallpolicy --namespace app
 #kubectl delete networkpolicy  allow-nginx-product --namespace app
+#kubectl delete networkpolicy  allow-nginx-product2 --namespace app
 
-kubectl create namespace app2
+ku
 
 #kubectl run nginxtest --image=nginx 
-kubectl describe pod  web-5d56b796f-sgkr7
-kubectl delete pod nginxtest
+kubectl describe pod  nginx-6998bbffd7-xb9tn
+kubectl describe pod  nginx-2-549dbdf9d9-l577h --namespace app2
+#kubectl delete pod nginxtest
 
 kubectl exec -it nginxtest  -- /bin/sh  
 curl -i -X GET http://www.google.com
 
 nginx-6998bbffd7-ks6dz  product-5456646588-99hvf   web-785d549876-9v2qd
 
-kubectl exec -it nginx-6998bbffd7-ks6dz -- /bin/sh
-kubectl exec -it web-785d549876-9v2qd -- /bin/sh
-kubectl exec -it product-5456646588-99hvf -- /bin/sh
+kubectl exec -it nginx-6998bbffd7-mgbpg --namespace ap -- /bin/sh
+kubectl exec -it nginx-2-549dbdf9d9-lphjt --namespace app2 -- /bin/sh
+
 
 apt-get  update
 apt-get install wget
 
-wget -qO- --timeout=5 http://localhost
+wget -qO- --timeout=1  http://10.240.0.11                           #web
 
-wget -qO- --timeout=2 http://10.240.0.7   #nginx
-wget -qO- --timeout=2  http://10.240.0.17  #web
+wget -qO- --timeout=1 http://10.240.0.6/api/product/GetMetadata     #product
+wget -qO- --timeout=1 http://10.240.0.6/swagger/index.html          #product
+wget -qO- --timeout=1 http://10.240.0.6.app/swagger/index.html      #product
 
-wget -qO- --timeout=2 http://10.240.0.13/api/product/GetMetadata #product
-wget -qO- --timeout=2 http://10.240.0.13/swagger/index.html
+wget -qO- --timeout=1 http://10.240.0.9/swagger/index.html          # product2
+wget -qO- --timeout=1 http://10.240.0.9.app2/swagger/index.html     # product2
+
+curl -i -X GET http://10.240.0.22.app                                  #nginx 
+curl -i -X GET http://10.240.0.23.app2                                 #nginx2
+
+
+curl -i -X GET http://frontend.ap
+curl -i -X GET http://backend.app2
